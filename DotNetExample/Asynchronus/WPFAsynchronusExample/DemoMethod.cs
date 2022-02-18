16af59367bc61e0e79c6b71a2ab47d90cbdcf237
dnhieu1992 <dnhieu1992@gmail.com>
2022-02-19 00:41:25 +0700

    Asynchronus example

---
+++ DotNetExample/Asynchronus/WPFAsynchronusExample/DemoMethod.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFAsynchronusExample
+{
+    public class ProgressReportData
+    {
+        public string Url { get; set; }
+        public int Percentage { get; set; }
+    }
+    public class WebDataResult
+    {
+        public string Url { get; set; }
+        public int Length { get; set; }
+    }
+    public class DemoMethod
+    {
+        private readonly List<string> Address = new List<string> {
+            "https://stackoverflow.com/",
+            "https://www.c-sharpcorner.com/",
+            "https://vnexpress.net/",
+            "https://dantri.com.vn/",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.net.webclient?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.net.webclient.-ctor?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/?view=net-6.0",
+            "https://docs.microsoft.com/en-us/aspnet/core/?view=aspnetcore-6.0",
+            "https://docs.microsoft.com/en-us/aspnet/core/introduction-to-aspnet-core?view=aspnetcore-6.0",
+            "https://docs.microsoft.com/en-us/aspnet/core/fundamentals/choose-aspnet-framework?view=aspnetcore-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.run?view=net-6.0",
+            "https://www.pluralsight.com/guides/using-task-run-async-await",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.run?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.runsynchronously?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.whenany?view=net-6.0",
+            "https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1?view=net-6.0"
+        };
+        private readonly WebClient client = new WebClient();
+
+        public IEnumerable<string> DownloadWebsite()
+        {
+            List<string> results = new List<string>();
+            foreach (var address in Address)
+            {
+                var stringResult = $"{address} : {client.DownloadString(address)?.Length} {Environment.NewLine}";
+                results.Add(stringResult);
+            }
+
+            return results;
+        }
+
+        public async Task<IEnumerable<string>> DownloadWebsiteAsync(IProgress<ProgressReportData> progress, CancellationToken cancellationToken)
+        {
+            var total = Address.Count() > 0 ? Address.Count : 1;
+            List<string> results = new List<string>();
+            foreach (var address in Address)
+            {
+                var stringResult = await client.DownloadStringTaskAsync(address);
+                cancellationToken.ThrowIfCancellationRequested();
+                results.Add($"{address} : {stringResult?.Length} {Environment.NewLine}");
+                progress.Report(new ProgressReportData() { Url = address, Percentage = (results.Count * 100)/total});
+            }
+
+            return results;
+        }
+
+        public async Task<IEnumerable<WebDataResult>> DownloadWebsiteParallelAsync(IProgress<int> progress)
+        {
+            List<Task<WebDataResult>> WebDataResults = new List<Task<WebDataResult>>();
+            foreach (var address in Address)
+            {
+                WebDataResults.Add(Task.Run(() => GetStringOfWebsite(address)));
+            }
+            var result = await Task.WhenAll(WebDataResults);
+            return result;
+        }
+
+        private WebDataResult GetStringOfWebsite(string address)
+        {
+            WebClient webClient = new WebClient();
+            var result = webClient.DownloadString(address);
+            return new WebDataResult() { Url = address, Length = result.Length };
+        }
+    }
+}
---
+++ DotNetExample/Asynchronus/WPFAsynchronusExample/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAsynchronusExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DemoMethod demoMethod = new DemoMethod();
+        CancellationTokenSource source = new CancellationTokenSource();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Normal_Excution_Click(object sender, RoutedEventArgs e)
+        {
+            txtResults.Text = "";
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var results = demoMethod.DownloadWebsite();
+            stopWatch.Stop();
+            PrintData(results);
+            txtResults.Text += $"Time consume: {stopWatch.ElapsedMilliseconds}";
+        }
+
+        private async void Async_Excute_Click(object sender, RoutedEventArgs e)
+        {
+            txtResults.Text = "";
+            try
+            {
+                var progress = new Progress<ProgressReportData>();
+                progress.ProgressChanged += ReportProgress;
+
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                var results = await demoMethod.DownloadWebsiteAsync(progress, source.Token);
+                stopWatch.Stop();
+                PrintData(results);
+                txtResults.Text += $"Time consume: {stopWatch.ElapsedMilliseconds}";
+            }
+            catch(Exception ex)
+            {
+                txtResults.Text += $"The request was cancelled.{Environment.NewLine}";
+            }
+        }
+
+        private async void Async_Parallel_Excute_Click(object sender, RoutedEventArgs e)
+        {
+            progressBar.Value = 0;
+            var progress = new Progress<int>(x => progressBar.Value = x);
+            txtResults.Text = "";
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var results = await demoMethod.DownloadWebsiteParallelAsync(progress);
+            stopWatch.Stop();
+            foreach(var item in results)
+            {
+                txtResults.Text += $"{item.Url}: {item.Length} {Environment.NewLine}";
+            }
+            txtResults.Text += $"Time consume: {stopWatch.ElapsedMilliseconds}";
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            source.Cancel();
+        }
+
+        private void PrintData(IEnumerable<string> results)
+        {
+            foreach (var item in results)
+            {
+                txtResults.Text += item;
+            }
+        }
+
+        private void ReportProgress(object sender, ProgressReportData e)
+        {
+            progressBar.Value = e.Percentage;
+            txtResults.Text += $"{e.Url} {Environment.NewLine}";
+        }
+    }
+}
